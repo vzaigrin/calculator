cf7c60eba4ab68a626c5f2e73e2c98c889ab1078
Vadim Zaigrin <vzaigrin@yandex.ru>
2019-05-29 13:02:43 +0300

    Add tests for Lexical Analyzer in Scala project

--- Scala/calculator/build.sbt
+++ Scala/calculator/build.sbt
@@ -3,3 +3,5 @@ name := "calculator"
 version := "1.0"
 
 scalaVersion := "2.12.8"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.5" % "test"
---
+++ Scala/calculator/src/test/scala/LexerTest.scala
@@ -0,0 +1,38 @@
+import calculator._
+import org.scalatest.FunSuite
+
+class LexerTest extends FunSuite {
+  test("Lexer.parse simple expression") {
+    assert(Lexer().parse("x = y * 2") === List(
+      Identifier("x"), Operator('=', "assign"), Identifier("y"), Operator('*', "multiply"), Number(2.0), End()
+    ))
+  }
+
+  test("Lexer.parse multiline expression") {
+    assert(Lexer().parse("y = 2 * Pi\n x = y + 2") === List(
+      Identifier("y"), Operator('=', "assign"), Number(2.0), Operator('*', "multiply"), Constant("Pi", math.Pi), End(),
+      Identifier("x"), Operator('=', "assign"), Identifier("y"), Operator('+', "plus"), Number(2.0), End()
+    ))
+  }
+
+  test("Lexer.parse multiline expression with comments") {
+    assert(Lexer().parse("y = 2 * Pi # first line \n x = y + 2# second line") === List(
+      Identifier("y"), Operator('=', "assign"), Number(2.0), Operator('*', "multiply"), Constant("Pi", math.Pi), End(),
+      Identifier("x"), Operator('=', "assign"), Identifier("y"), Operator('+', "plus"), Number(2.0), End()
+    ))
+  }
+
+  test("Lexer.parse multiline expression with continue") {
+    assert(Lexer().parse("y = 2 * Pi \\ \n + 2") === List(
+      Identifier("y"), Operator('=', "assign"), Number(2.0), Operator('*', "multiply"), Constant("Pi", math.Pi),
+      Continue(), Operator('+', "plus"), Number(2.0), End()
+    ))
+  }
+
+  test("Lexer.parse multiline expression with continue and comment") {
+    assert(Lexer().parse("y = 2 * Pi \\# first line\\\n + 2 # second line") === List(
+      Identifier("y"), Operator('=', "assign"), Number(2.0), Operator('*', "multiply"), Constant("Pi", math.Pi),
+      Continue(), Operator('+', "plus"), Number(2.0), End()
+    ))
+  }
+}
